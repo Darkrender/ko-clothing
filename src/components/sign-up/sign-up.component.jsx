4fff8bfb400b1754a1e6c9c9a603bbf031712735
Darkrender <jimmiegisclair@gmail.com>
2020-03-30 12:59:59 -0500

    create form-input, sign-in, and sign-up components and their container page

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>SIGN UP FORM</div>
+    );
+  }
+}
+
+export default SignUp;
