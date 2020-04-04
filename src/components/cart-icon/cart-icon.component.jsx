9f824f31c3509a9167b2000f87b749c29dda79c5
Darkrender <jimmiegisclair@gmail.com>
2020-04-04 14:14:15 -0500

    update cart-icon component to display the number of items in the cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,15 +4,19 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, numItemsInCart }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{numItemsInCart}</span>
   </div>
 );
+
+const mapStateToProps = state => ({
+  numItemsInCart: state.cart.numItemsInCart
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -3,7 +3,8 @@ import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
-  cartItems: []
+  cartItems: [],
+  numItemsInCart: 0
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,7 +12,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {...state, hidden: !state.hidden};
     case CartActionTypes.ADD_ITEM:
-      return {...state, cartItems: addItemToCart(state.cartItems, action.payload)};
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+        numItemsInCart: state.numItemsInCart + 1
+      };
     default:
       return state;
   }
