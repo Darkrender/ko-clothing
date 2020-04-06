ac733e45f02052dad1910afb4ee458cc56de26a0
Darkrender <jimmiegisclair@gmail.com>
2020-04-06 12:44:39 -0500

    refactor header component to use memoized selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,14 +2,19 @@ import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+)
+
 export const selectCartItems = createSelector(
   [selectCart],
-  (cart) => cart.cartItems
+  cart => cart.cartItems
 );
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
-  (cartItems) =>
+  cartItems =>
     cartItems.reduce(
       (accumulatedQuantity, cartItem) =>
         accumulatedQuantity + cartItem.quantity,
